d5d50a64883289bee8891db9d85e6eec03259f99
Md Junaead <junaead12364@gmail.com>
2025-07-12 19:40:53 +0600

    Loss 200

--- students/views.py
+++ students/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 
 
 def students(request):
-    return HttpResponse('<h2> Hello Traders </h2>')
+    return HttpResponse('<h2> Hello Traders, today loss 200 taka </h2>')
