e4109c8a2df81283b8d616db03ee008c43295f70
Md Junaead <junaead12364@gmail.com>
2025-07-20 14:03:34 +0600

    soft

--- students/views.py
+++ students/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 
 
 def students(request):
-    return HttpResponse('<h2> Last day my BOU Win 100$ in giveway </h2>')
+    return HttpResponse('<h2> Tomorrow is interview </h2>')
