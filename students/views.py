c886317f09ef2ddc6132b16b67a757ef1dab4e27
Md Junaead <junaead12364@gmail.com>
2025-07-26 09:31:28 +0600

    icc Interview

--- students/views.py
+++ students/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 
 
 def students(request):
-    return HttpResponse('<h2> Tomorrow is interview, Interview was good but make lots of mistake, I hope to get the job </h2>')
+    return HttpResponse('<h2> Tomorrow is interview, Interview was good but make lots of mistake, I hope to get the job, ICC </h2>')
